ee59609fc8f4e34f059fe36a574a357ea390b2c8
HuYingTran <huynh.technical@gmail.com>
2023-04-27 23:12:26 +0700

    del

--- code_matlab/untitled3.m
+++
@@ -1,6 +0,0 @@
-arduinoObj = arduino("COM10",9600);
-
-serialdevObj = device(arduinoObj,'SerialPort',1,'BaudRate', 115200, 'DataBits',8, 'StopBits', 1, 'Parity', 'even')
-write(serialdevObj,256,'uint16');
-numBytes = serialdevObj.NumBytesAvailable
-read(serialdevObj,1,'uint16')
--- code_matlab/xuLyTinHieu.m
+++
@@ -1,9 +0,0 @@
-y = xlsread('E:\DoAn\Sensor\xuLy_soLieu.xlsx',1,'D50:D290')
-length(y);
-fs = 5;
-t=0:1/fs:20-1/fs;
-x=fft(y);
-length(t)
-x=abs(x);
-x=[x(1)-39000;x(2:240)]
-plot(x(2:240))
